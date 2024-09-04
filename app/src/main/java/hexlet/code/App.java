67a2e3e936e0c7460acf65fdaa660ced358a074c
DenisDanilov1 <denchik199819981998@gmail.com>
2024-09-04 13:42:49 +0400

    uploaded the changes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,86 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.controller.UrlController;
+import hexlet.code.repository.BaseRepository;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 
-public final class App {
-    public static Javalin getApp() {
-        var app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
-        });
-        app.get("/welcome", ctx -> ctx.result("Hello World"));
-        return app;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+import io.javalin.rendering.template.JavalinJte;
+import gg.jte.resolve.ResourceCodeResolver;
+
+import static hexlet.code.repository.BaseRepository.dataSource;
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
     }
-    public static void main(String[] args) {
-        Javalin app = getApp();
-        app.start(7070);
+
+    public static String readResourceFile(String filename) throws IOException {
+        InputStream inputStream = App.class.getClassLoader().getResourceAsStream(filename);
+        if (inputStream == null) {
+            throw new RuntimeException("Not found");
+        } else {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(System.getenv().getOrDefault("JDBC_DATABASE_URL",
+                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;"));
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("schema.sql");
+
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        BaseRepository.dataSource = dataSource;
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte(createTemplateEngine()));
+        });
+
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=utf-8");
+        });
+
+        app.get(NamedRoutes.rootPath(), UrlController::index);
+        app.post(NamedRoutes.urlsPath(), UrlController::create);
+        app.get(NamedRoutes.urlsPath(), UrlController::showUrls);
+        app.get(NamedRoutes.urlPath("{id}"), UrlController::showUrl);
+        app.post(NamedRoutes.urlCheckPath("{id}"), UrlController::check);
+
+        return app;
     }
 }
---
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -0,0 +1,111 @@
+package hexlet.code.controller;
+
+import hexlet.code.dto.BasePage;
+import hexlet.code.dto.UrlPage;
+import hexlet.code.dto.UrlsPage;
+import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.repository.UrlRepository;
+import hexlet.code.util.NamedRoutes;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.Unirest;
+import kong.unirest.core.UnirestException;
+import org.jsoup.Jsoup;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Collections;
+
+public class UrlController {
+    public static void index(Context ctx) {
+        var page = new BasePage();
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("type"));
+        ctx.render("index.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void create(Context ctx) throws SQLException {
+        var url = ctx.formParamAsClass("url", String.class).getOrDefault(null);
+        URL addedUrl = null;
+
+        try {
+            addedUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("type", "danger");
+            ctx.redirect("/");
+        }
+
+        if (addedUrl != null) {
+            String protocol = addedUrl.getProtocol();
+            String authority = addedUrl.getAuthority();
+            var host = String.format("%s://%s", protocol, authority);
+            var newUrl = new Url(host);
+
+            if (UrlRepository.findByName(host).isEmpty()) {
+                UrlRepository.save(newUrl);
+                ctx.sessionAttribute("flash", "Страница успешно добавлена");
+                ctx.sessionAttribute("type", "success");
+            } else {
+                ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.sessionAttribute("type", "danger");
+            }
+            ctx.redirect("/urls");
+        }
+    }
+
+    public static void showUrl(Context ctx) throws SQLException {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var url = UrlRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Url not found"));
+
+        var checks = UrlCheckRepository.getChecks(id);
+        var page = new UrlPage(url, checks);
+
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("type"));
+        ctx.render("urls/show.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void showUrls(Context ctx) throws SQLException {
+        var urls = UrlRepository.getEntities();
+        var checks = UrlCheckRepository.getAllChecks();
+        var page = new UrlsPage(urls, checks);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("type"));
+        ctx.render("urls/index.jte", Collections.singletonMap("page", page));
+
+    }
+
+    public static void check(Context ctx) throws SQLException {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var url = UrlRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Id " + id + " not found"));
+        String name = url.getName();
+
+        try {
+            HttpResponse<String> resp = Unirest.get(name).asString();
+            var statusCode = resp.getStatus();
+            var parse = Jsoup.parse(resp.getBody());
+            var title = parse.title();
+            var firstEl = parse.selectFirst("h1");
+            var h1 = (firstEl == null ? null : firstEl.ownText());
+            var forDesc = parse.selectFirst("meta[name=description]");
+            var desc = (forDesc == null ? null : forDesc.attr("content"));
+            var check = new UrlCheck(statusCode, title, h1, desc, id);
+
+            UrlCheckRepository.save(check);
+            ctx.sessionAttribute("flash", "Проверка прошла успешно");
+            ctx.sessionAttribute("type", "success");
+            ctx.redirect(NamedRoutes.urlPath(id));
+        } catch (UnirestException e) {
+            ctx.sessionAttribute("flash", "Не удалось проверить");
+            ctx.sessionAttribute("type", "danger");
+            ctx.redirect(NamedRoutes.urlPath(id));
+        }
+    }
+}
