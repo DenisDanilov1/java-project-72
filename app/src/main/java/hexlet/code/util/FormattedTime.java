9da22c9c30e621ca2689641c376e30c6b818d76a
DenisDanilov1 <denchik199819981998@gmail.com>
2024-09-30 13:19:50 +0400

    changed the formatedtime

--- app/src/main/java/hexlet/code/util/FormattedTime.java
+++ app/src/main/java/hexlet/code/util/FormattedTime.java
@@ -1,11 +1,11 @@
 package hexlet.code.util;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FormattedTime {
-    public static String get(Timestamp time) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        return dtf.format(time.toLocalDateTime());
+    public static String format(LocalDateTime time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return time.format(formatter);
     }
 }
